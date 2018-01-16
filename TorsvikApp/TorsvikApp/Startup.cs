3d15123d6da5e060d44a192ccf23761351e6904a
André Torsvik <andret14@student.uia.no>
2018-01-16 19:33:00 +0100

    first commit

---
+++ TorsvikApp/TorsvikApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TorsvikApp.Startup))]
+namespace TorsvikApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
